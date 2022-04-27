7054da54da60601cad1465a03d05e4a62cf6228e
Elyas <elyas.ghausi@gmail.com>
2022-04-27 19:22:37 +0400

    Formlar kecdik

--- blogs/models.py
+++ blogs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(verbose_name="Title", max_length=100)
     body = models.TextField(verbose_name="Body")
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateField(auto_now_add=True)
 
     def __str__(self) -> str:
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import AddBlogForm
+from .models import Blog
 # Create your views here.
+
+
+def index(request):
+    blogForm = AddBlogForm()
+    blogs  = Blog.objects.all()
+    
+    if request.method == 'POST':
+        blogForm = AddBlogForm(request.POST)
+        if blogForm.is_valid():
+            blogForm.save()
+            return redirect('/')
+        else:
+            blogForm = AddBlogForm(request.POST)
+
+    context = {
+        "blogForm": blogForm,
+        "blogs": blogs,
+    }
+    return render(request, 'index.html', context)
